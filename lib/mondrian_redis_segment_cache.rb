58eb1edb5ceba138c678bda2b64041c06d71f5ee
Brandon Dewitt <brandonsdewitt@gmail.com>
2014-06-19 17:00:24 -0600

    require cache

--- lib/mondrian_redis_segment_cache.rb
+++ lib/mondrian_redis_segment_cache.rb
@@ -1,6 +1,7 @@
 require 'java'
 require 'java_to_base64'
 require 'mondrian-olap'
+require 'mondrian_redis_segment_cache/cache'
 require 'mondrian_redis_segment_cache/created_event'
 require 'mondrian_redis_segment_cache/deleted_event'
 require 'mondrian_redis_segment_cache/segment_body'
